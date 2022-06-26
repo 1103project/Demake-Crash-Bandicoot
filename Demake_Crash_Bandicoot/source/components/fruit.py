ede4bbed0bea65101be0ffc1e633f2b6cf8047b4
Greengoldstone <97942522+Greengoldstone@users.noreply.github.com>
2022-06-26 10:48:43 +0800

    fruit重写

--- Demake_Crash_Bandicoot/source/components/fruit.py
+++ Demake_Crash_Bandicoot/source/components/fruit.py
@@ -1,34 +1,40 @@
 import pygame
-from source import tools, setup
-from source import contants as C
 
 
-class Fruit(pygame.sprite.Sprite):#水果类继承精灵类
-    def __init__(self):
+class Fruit(pygame.sprite.Sprite):
+    #定义构造函数
+    def __init__(self, fruit_filename, fruit_location):
+        #调用父类来初始化子类
         pygame.sprite.Sprite.__init__(self)
-        self.frames = []
-        self.frame_index = 0
-        frame_rects = [()]#水果位置xy宽高
-        self.load_frame(frame_rects)
-        self.image = self.frames[self.frame_index]
+        #加载水果图片
+        self.image = pygame.image.load(fruit_filename)
+        #获取图片rect区域
         self.rect = self.image.get_rect()
-        self.rect.x = 500
-        self.rect.y = 10
-        self.timer = 0
-
-    def load_frame(self, frame_rects):
-        sheet = setup.GRAPHICS['']#图片来源
-        for frame_rect in frame_rects:
-            self.frames.append(tools.get_image(sheet, *frame_rect, (0, 0, 0), C.BG_MULTI))
-            self.current_time = pygame.time.get_ticks()
-            frame_duraction = []#buff停留时间
-
-        if self.timer == 0
-            self.timer = self.current_time
-        elif self.current_time - self.timer > frame_duraction[self.frame_index]:
-            self.frame_index = 1
-            self.timer = self.current_time
-
-        self.image = self.frames[self.frame_index]
-
+        #设置图片位置
+        self.rect.topleft = fruit_location
+    #屏幕画出水果
+    def fruit_draw(self):
+        screen.blit(self.image, self.rect)
+    #定义碰撞
+    def fandb_crash(self, other, fruit_filename):
+        crash_result = pygame.sprite.collide_rect(self, other)
+        if crash_result:
+            #原来水果图变透明
+            transparent = (0, 0, 0, 0)
+            self.image.fill(transparent)
+            #左上角画出水果buff
+            fruit_image = pygame.image.load(fruit_filename)
+            fruit_image = pygame.transform.scale(fruit_image, (10, 10))
+            screen.blit(fruit_image, (0, 0))
+            #定义时间
+            time_begin = 0
+            time_now = pygame.time.get_ticks()
+            time_begin = time_now
+            #buff维持10秒，10秒后左上角水果图片变透明
+            if time_now - time_begin >= 1000:
+                fruit_image.fill(transparent)
+            else:
+                pass
+        else:
+            pass
 
